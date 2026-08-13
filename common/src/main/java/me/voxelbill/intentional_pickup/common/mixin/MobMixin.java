dee30174b0576c482276fca1e8d59d763be21df5
VoxelBill <bjtmastermind@gmail.com>
2026-08-12 20:39:33 -0400

    Added support for pickup whitelist and blacklist controlled from item tags. Implements #1

--- common/src/main/java/me/voxelbill/intentional_pickup/common/mixin/MobMixin.java
+++ common/src/main/java/me/voxelbill/intentional_pickup/common/mixin/MobMixin.java
@@ -5,9 +5,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import me.voxelbill.intentional_pickup.common.tags.ModItemTags;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
 
 @Mixin(Mob.class)
 public class MobMixin {
@@ -20,7 +23,16 @@ public class MobMixin {
         ),
         cancellable = true
     )
-    public void mob_intentional_pickup$pickUpItemInject(ServerLevel level, ItemEntity entity, CallbackInfo ci) {
+    public void intentional_pickup$pickUpItemInject(ServerLevel level, ItemEntity entity, CallbackInfo ci) {
+        RegistryAccess access = ((Mob)(Object)this).level().registryAccess();
+        boolean hasWhitelist = !ModItemTags.isTagEmpty(ModItemTags.PICKUP_WHITELIST, access);
+        boolean hasBlacklist = !ModItemTags.isTagEmpty(ModItemTags.PICKUP_BLACKLIST, access);
+        ItemStack item = entity.getItem();
+
+        if ((hasWhitelist && !item.is(ModItemTags.PICKUP_WHITELIST)) || (hasBlacklist && item.is(ModItemTags.PICKUP_BLACKLIST))) {
+            ci.cancel();
+        }
+
         if (entity.getOwner() == null) {
             ci.cancel();
         }
